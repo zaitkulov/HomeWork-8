35dac4c2dd11a9997a080e194fb39c4ae91bd42b
zaitkulov <zaitkulov@gmail.com>
2024-01-08 14:32:47 +0500

    HomeWork-8-2. Разработка и тестирование иерархии классов животных

--- HomeWork-8/main.swift
+++ HomeWork-8/main.swift
@@ -43,4 +43,72 @@ startFighter1()
 print("\n")
 startFighter2()
 
+// 2.Задание: Разработка и тестирование иерархии классов животных
+
+class Animal {
+    let name: String
+    
+     init(name: String) {
+        self.name = name
+    }
+    var type: String {
+        return "Животное"
+    }
+    func sleep() -> String {
+        return "\(type) \(name) спит"
+    }
+}
+class Cat: Animal {
+    let sound: String
+    
+    init(name: String, sound: String = "Мяу") {
+        self.sound = sound
+        super.init(name: name)
+        
+    }
+    override var type: String {
+        return "Кот"
+    }
+    override func sleep() -> String {
+        return "\(super.sleep()) и мурлычет"
+    }
+    func play() {
+        print ("\(type) \(name) играет")
+    }
+}
+class Dog: Animal {
+    let sound: String
+    
+    init(name:String, sound: String = "Гав") {
+        self.sound = sound
+        super.init(name: name)
+    }
+    override var type: String {
+        return "Собака"
+    }
+    override func sleep() -> String {
+        return "\(super.sleep()) и храпит"
+    }
+    func play() {
+        print ("\(type) \(name) играет")
+    }
+}
+let cat1 = Cat(name: "Маркиз", sound: "Мяу")
+let dog1 = Dog(name: "Джек", sound: "Гав")
+print(cat1.type)
+print(dog1.type)
+
+print(cat1.sleep())
+print(dog1.sleep())
+
+cat1.play()
+dog1.play()
+
+let cat2 = Cat(name: "Вася")
+print(cat2.type)
+print(cat2.sound)
+
+print("Звук кота: \(cat1.sound)")
+print ("Звук собаки: \(dog1.sound)")
+
 
