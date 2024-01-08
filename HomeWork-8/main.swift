b7dad1b1a6b390743387400c77c4a6eb1e6ddd68
zaitkulov <zaitkulov@gmail.com>
2024-01-08 14:04:28 +0500

    HomeWork-8. Разработка и тестирование навыков бойцов

--- HomeWork-8/main.swift
+++ HomeWork-8/main.swift
@@ -7,5 +7,40 @@
 
 import Foundation
 
-print("Hello, World!")
+// 1.Задание: Разработка и тестирование боевой системы в ролевой игре
+
+class Fighter {
+    func attack() {
+        print ("Обычная атака бойца")
+    }
+    func useUltimateAbility() {
+        print ("У бойца нет уникальной способности")
+    }
+}
+class Warrior: Fighter {
+    override func useUltimateAbility() {
+        print ("Warrior использует Супер Удар!")
+    }
+}
+class Mag: Fighter {
+    override func useUltimateAbility() {
+        print ("Маг использует Ультимативное заклинание!")
+    }
+}
+func startFighter1() {
+    let warrior = Warrior()
+    warrior.attack()
+    warrior.useUltimateAbility()
+    warrior.attack()
+}
+func startFighter2() {
+    let mag = Mag()
+    mag.attack()
+    mag.useUltimateAbility()
+    mag.attack()
+}
+startFighter1()
+print("\n")
+startFighter2()
+
 
